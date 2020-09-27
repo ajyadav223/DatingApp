84dd24f2822c9cf0e1308421b6224474cfeba239
Ajay <you@example.comajyadav223@gmail.com>
2020-09-27 17:08:23 +0530

    End Of Sec 26 Routing in Angular

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,24 +5,36 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import {FormsModule} from '@angular/forms';
 import {AuthService} from './_services/auth.service';
+import {RouterModule } from '@angular/router';
+
+
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import {ErrorInterceptorProvider} from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ListsComponent } from './lists/lists.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import {appRoutes} from './routes';
+
 @NgModule({
-  declarations: [			
+  declarations: [						
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      ListsComponent,
+      MemberListComponent,
+      MessagesComponent
    ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ 
       AuthService,
